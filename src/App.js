9e567e1cc2161db6712991b7c3d11ddd72802eca
donadzai <kieuduydoan2k2@gmail.com>
2023-09-14 16:11:28 +0700

    làm preview avatar

--- src/App.js
+++ src/App.js
@@ -4,54 +4,28 @@ import styles from './App.module.scss';
 
 const cx = classNames.bind(styles);
 
-const tabs = ['posts', 'comments', 'albums'];
-
 function App() {
-    const [type, setType] = useState('posts');
-
-    const [content, setContent] = useState('');
-
-    const [show, setShow] = useState(false);
-
-    const handleGoToTop = () => window.scrollTo(0, 0);
+    const [image, setImage] = useState('');
 
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/${type}`)
-            .then((res) => res.json())
-            .then((data) => setContent(data));
-    }, [type]);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            console.log(window.scrollY);
-            setShow(window.scrollY > 200);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
         return () => {
-            window.removeEventListener('scroll', handleScroll);
+            if(!image) {
+                return;
+            }
+            URL.revokeObjectURL(image.url);
         };
-    }, []);
+    }, [image]);
+
+    const handleChange = (e) => {
+        const files = e.target.files[0];
+        files.url = URL.createObjectURL(files);
+        setImage(files);
+    };
 
     return (
         <div className={cx('wrapper')}>
-            {tabs.map((tab) => (
-                <button
-                    onClick={() => setType(tab)}
-                    style={tab === type ? { color: '#fff', backgroundColor: '#000' } : {}}
-                    key={tab}
-                >
-                    {tab}
-                </button>
-            ))}
-
-            {content &&
-                content.map((item) => {
-                    return <li key={item.id}>{item.title || item.name}</li>;
-                })}
-
-            {show && <button onClick={handleGoToTop} style={{ position: 'fixed', bottom: 20, right: 20 }}>GO TO TOP</button>}
+            <input type="file" onChange={handleChange} />
+            {image && <img style={{ width: 500, height: 500, objectFit: 'cover' }} src={image.url} alt="anh" />}
         </div>
     );
 }
